c9a8e7f6210cdd863cc6968a0b61e0811d71dabb
alazad214 <alazad214@gmail.com>
2025-04-18 21:39:41 +0600

    dynamic type change

---
+++ problem_solving_part1.dart/dynamic_type_change.dart
@@ -0,0 +1,10 @@
+void main(){
+  dynamic value='Hello';
+  print(value);
+value=12345645;
+print(value);
+value=false;
+print(value);
+}
+
+// ব্যাখ্যা: dynamic টাইপ যেকোনো টাইপের মান রাখতে পারে — যেমন String, int, bool ইত্যাদি।
