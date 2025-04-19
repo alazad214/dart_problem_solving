4d5063e2dbd186943bddd3ba77fbf9cf91d4bcac
alazad214 <alazad214@gmail.com>
2025-04-19 22:31:12 +0600

    int<>string convert program

---
+++ problem_solving2/int_to_string.dart
@@ -0,0 +1,5 @@
+void main() {
+  int number = 50;
+  String text = number.toString();
+  print("text type: ${text.runtimeType}");
+}
---
+++ problem_solving2/string_to_int.dart
@@ -0,0 +1,5 @@
+void main() {
+  String input = "100";
+  int parsed = int.parse(input);
+  print("Parsed int: $parsed");
+}
